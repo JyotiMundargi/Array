306782fe2f9a0c588d4e855b50363ef416a92898
Jyoti Mundargi <163743624+JyotiMundargi@users.noreply.github.com>
2025-09-11 21:40:35 +0530

    Add files via upload

---
+++ temp1.cpp
@@ -0,0 +1,15 @@
+//simple array 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int temperature[5]={14,67,89,45,67};
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<"the temperature is "<<temperature[i]<<" degree celcius \n";
+    }
+
+    return 0;
+}
